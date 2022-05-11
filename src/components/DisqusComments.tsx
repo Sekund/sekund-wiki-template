5f27218d3b0e1bf1ed13c06a4529ed6736069d67
Candide Kemmler <hi@candide.me>
2022-05-11 18:26:04 +0200

    debugging disqus integration

--- src/components/DisqusComments.tsx
+++ src/components/DisqusComments.tsx
@@ -9,11 +9,18 @@ type DisqusProps = {
 };
 
 const DisqusComments = ({ id, title }: DisqusProps) => {
+  console.log(
+    'disqus comments',
+    id,
+    title,
+    `${WIKI_DOMAIN}/${id}/${slugify(title)}`
+  );
   const disqusShortname = 'digital-society';
   const disqusConfig = {
     url: `${WIKI_DOMAIN}/${id}/${slugify(title)}`,
     identifier: id, // Single post id
     title, // Single post title
+    language: 'fr',
   };
   return <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />;
 };
