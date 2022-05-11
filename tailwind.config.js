a244388e10d52dc2155d6cefd53b23073a42f011
Candide Kemmler <hi@candide.me>
2022-05-11 12:40:51 +0200

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable global-require */
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontSize: {
