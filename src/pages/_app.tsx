dfae9043d86864847308d314d6f097ee24a71144
Candide Kemmler <hi@candide.me>
2022-05-17 15:43:30 +0200

    fixes youtube embed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { withDarkMode } from '@/components/ThemedApp';
 
 import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1"
+        key="viewport"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default withDarkMode(MyApp);
