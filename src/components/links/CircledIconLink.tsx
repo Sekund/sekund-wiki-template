d6860ab73df101e2c614be7ababc08ce60d8116a
Candide Kemmler <hi@candide.me>
2022-05-20 14:11:57 +0200

    fix link color

--- src/components/links/CircledIconLink.tsx
+++ src/components/links/CircledIconLink.tsx
@@ -1,6 +1,8 @@
-import { ReactFragment, useState } from 'react';
+import { ReactFragment, useContext, useState } from 'react';
 
 import Link from 'next/link';
+
+import { DarkModeContext } from '@/components/ThemedApp';
 
 export type LinkProps = {
   children: ReactFragment;
@@ -10,19 +12,28 @@ export type LinkProps = {
 };
 
 const CircledIconLink = ({ label, href, children, target }: LinkProps) => {
-  const [color, setColor] = useState('#cbd5e0');
+  const { darkMode } = useContext(DarkModeContext);
+  const [color, setColor] = useState(darkMode ? '#cbc5e0' : '#718096');
+
+  function toggleHighlight(hl: boolean) {
+    if (darkMode) {
+      setColor(hl ? '#f7fafc' : '#cbc5e0');
+    } else {
+      setColor(hl ? '#1a202c' : '#718096');
+    }
+  }
 
   function Widget() {
     return (
       <a
         className={`relative inline-flex space-x-1 cursor-pointer icon-link no-underline`}
         href={href}
-        onMouseOver={() => setColor('#718096')}
-        onMouseLeave={() => setColor('#cbd5e0')}
+        onMouseOver={() => toggleHighlight(true)}
+        onMouseLeave={() => toggleHighlight(false)}
         target={target}
         style={{ color }}
       >
-        <span className="text-gray-600">{label}</span>
+        <span style={{ color }}>{label}</span>
         <span
           className={`flex flex-col items-center justify-center border rounded-full`}
           style={{ width: '1.5em', height: '1.5em', borderColor: color }}
